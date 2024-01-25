0077ef9d7891c20b2275ed62747400efdd310445
Erfan-Edalati <erfannstreet@gmail.com>
2024-01-25 13:17:35 +0330

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_Method
+{
+    public static class Helper
+    {
+        // GetFullName Method -- return string
+        public static string GetFullName()
+        {
+            return "Erfan Edalati";
+        }
+
+
+        // GetWelcomeMessage Method -- return string
+        public static string GetWelcomeMessage()
+        {
+            return "Welcome " + "Erfan Edalati";
+        }
+
+
+        //GetProfile Method with Input -- return string
+        public static string GetProfile(string name, string lastName)
+        {
+            return name + " " + lastName;
+        }
+
+
+        //GetNumber Method -- return int
+        public static int GetNumber()
+        {
+            return 10;
+        }
+
+
+        //Sum Method with Input -- return int
+        public static int Sum(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            return result;
+        }
+
+        //GetIsThere Method -- return bool
+        public static bool GetIsThere()
+        {
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Learn_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // GetFullName
+
+            string fullName = Helper.GetFullName();
+
+            Console.WriteLine(fullName);
+
+            // GetWelcomeMessage
+
+            string welcomeMessage = Helper.GetWelcomeMessage();
+
+            Console.WriteLine(welcomeMessage);
+
+            // GetProfile
+
+            string profile = Helper.GetProfile(name: "Erfan", lastName: "Edalati");
+
+            Console.WriteLine(profile);
+
+            // GetNumber
+
+            int number = Helper.GetNumber();
+
+            Console.WriteLine(number);
+
+            // Sum
+
+            int sum = Helper.Sum(num1: 10, num2: 20);
+
+            Console.WriteLine(sum);
+
+            // GetIsThere
+
+            bool isThere = Helper.GetIsThere();
+
+            Console.WriteLine(isThere);
+        }
+    }
+}
